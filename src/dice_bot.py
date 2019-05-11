9d3fcedc373d8480b68caa70745d37315a651ad2
dustin rosebery <dustin.rosebery@asu.edu>
2019-05-11 11:59:53 -0700

    converted to regex parser

--- src/dice_bot.py
+++ src/dice_bot.py
@@ -17,6 +17,9 @@ import command_parser as cp
 
 # Starting character the bot looks for to process a server message
 botChar = "!"
+
+# Bot owners discord name, set this for kill command
+botOwner = "hdbag"
  
 # BOT client events
 client = discord.Client()
@@ -27,43 +30,46 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Ready To Roll!")
+    game = discord.Game("D&D")
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 """
 	Process messages from the discord server
 """
 @client.event
 async def on_message(message):
-	# set message parameters
-	author = str(message.author)
-	nickname = str(message.author.nick)
-	if nickname == "None":
-		nickname = author.split("#")[0]
-
+	# set nickname
+	nickname = str(message.author).split("#")[0] if str(message.author.nick) == "None" else str(message.author.nick)
 	# don't let the bot talk to itself
 	if message.author == client.user:
 		return
-	# kill command
-	if message.content == "!!!":
-		sys.exit(0)
-	# process message
+	# only act on botChar
 	if message.content[0] == botChar:
+		
+		# process command
 		print("\nMessage Recieved from " + nickname + ": " + message.content)
-		command = cp.Command(message.content)
+		command = cp.Command()
+		command.parseCommand(message.content)
 
 		# send processed command message
 		if not command.isValid:
-			await message.channel.send(command.errorMsg)
+			await message.channel.send(nickname + ": " + command.errorMsg)
 			print("Sent Error Message to: " + nickname + " -> " + command.errorMsg)
 			return
-		if command.action == "help":
+		elif command.action == "help":
 			await message.channel.send(command.helpMsg)
 			print("Sent Help Message to: " + nickname)
-			return
-		if command.action == "roll":
-			rollMsg = nickname + " rolled: " + str(command.rolls) + " -> " + str(command.total)
+		# only let bot owner terminate
+		elif command.action == "kill" and str(message.author).split("#")[0] == botOwner:
+			await message.channel.send("Shutting Down")
+			await client.change_presence(status=discord.Status.offline, activity=None)
+			await client.close()
+			print("Dice Bot Shutdown")
+			system.exit(0)
+		elif command.action == "roll":
+			rollMsg = nickname + " rolled " + str(command.numRolls) + "d" + str(command.diceSize) + str(command.modifierSign) + str(command.modifier) + ": " + str(command.rolls) + " -> " + str(command.total)
 			await message.channel.send(rollMsg)
 			print(rollMsg)
-			return
 
 # Runs the bot using the unique discord generated auth token
 if __name__ == "__main__":
